a60f8ac474ac04a01ab27e0d944476797bf5bb38
SkippaT <tskipmitchell@yahoo.com>
2024-11-03 15:46:25 +1100

    Added fade to marquee

--- src/App.jsx
+++ src/App.jsx
@@ -7,52 +7,6 @@ import { TAGS } from './data.js';
 
 function App() {
 
-  const tags = [
-    "Upper one",
-    "Upper two",
-    "Upper three",
-    "Upper four",
-    "Upper five",
-    "Upper six",
-    "Upper seven",
-    "Upper eight",
-    "Upper nine",
-    "Upper ten",
-    "Upper eleven",
-    "Upper twelve",
-    "Upper thirteen",
-    "Upper fourteen",
-    "Upper fifteen",
-    "Upper sixteen",
-    "Upper seventeen",
-    "Upper eighteen",
-    "Upper nineteen",
-    "Upper twenty",
-  ]
-
-  const lowerMarquee = [
-    "Lower one",
-    "Lower two",
-    "Lower three",
-    "Lower four",
-    "Lower five",
-    "Lower six",
-    "Lower seven",
-    "Lower eight",
-    "Lower nine",
-    "Lower ten",
-    "Lower eleven",
-    "Lower twelve",
-    "Lower thirteen",
-    "Lower fourteen",
-    "Lower fifteen",
-    "Lower sixteen",
-    "Lower seventeen",
-    "Lower eighteen",
-    "Lower nineteen",
-    "Lower twenty",
-  ]
-
   const marqueeDuration = 60;
 
   return (
